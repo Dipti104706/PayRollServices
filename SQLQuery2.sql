074d7b31bf1adc34dfe9b10856e40aa86d46d34c
Dipti104706 <diptimayeebehura104@gmail.com>
2021-10-14 00:44:00 +0530

    [Dipti] added code to retrieve all data from table

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -12,3 +12,10 @@ CREATE TABLE employee_payroll(
 	Salary float,
 	StartDate date
 	);
+
+--------UC3 insert operation--------
+INSERT INTO employee_payroll VALUES('Akhshya','125245','2016/03/01') ,('Akhil','13335','2018/01/05'),('kabir','525245','2019/03/01');
+
+--------UC4-----------
+---Retrieve all data from employee_payroll
+select * from employee_payroll;
