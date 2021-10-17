b438fdf13ebecaf429c24c7d92ab5d037c89f1ec
Dipti104706 <diptimayeebehura104@gmail.com>
2021-10-17 23:18:52 +0530

    [Dipti] added code to Impliment ER diagram into payroll database

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -52,7 +52,7 @@ update employee_payroll set PhoneNumber = '7375787969',Address = 'Hyderabad', De
 
 ---Uc9---
 ---RenameColomn name in existing table(Salary renamed as Basic pay)---
-Exec sp_rename 'employee_payroll.Salary', 'BasicPay';
+Exec sp_rename 'employee_payroll.Salary', 'BasicPay','COLUMN';
 alter table employee_payroll add TaxablePay float, Deduction float,IncomeTax float,NetPay float;
 Update employee_payroll set Deduction = '4000' where Department = 'HR';
 Update employee_payroll set Deduction = '3000' where Department = 'Sales';
@@ -66,4 +66,71 @@ select * from employee_payroll;
 ------Create duplicate of person-------
 INSERT INTO employee_payroll(Name,BasicPay,StartDate,Gender,PhoneNumber,Department,Address,TaxablePay,Deduction,IncomeTax,NetPay) VALUES('Terissa','525245','2018/03/01','F','7345787969','Sales','Chennai','1000','4000','200','522245');
 INSERT INTO employee_payroll(Name,BasicPay,StartDate,Gender,PhoneNumber,Department,Address,TaxablePay,Deduction,IncomeTax,NetPay) VALUES('Terissa','525245','2018/03/01','F','7345787969','Marketing','Chennai','0','0','0','0');
----------Two ids for terissa created for single person, so to remove this we use ER disgram-------
+---------Two ids for terissa created for single person, so to remove this we use ER diagram-------
+
+-------------UC11-------------
+------By implimentaing ER, redoing UC 7--------------
+-----Create table--------Company Details---
+create table company
+(
+company_Id int identity(1,1) primary key,
+company_name varchar(255)
+)
+Insert into company values('Microsoft'),('Google')
+select * from company
+
+--Create table--Employee Details---
+create table Employee
+(
+EmployeeId int identity(1,1) primary key,
+EmployeeName varchar(255),
+Gender char(1),
+EmployeePhoneNumber bigint,
+EmployeeAddress varchar(255),
+StartDate date,
+CompanyId int
+Foreign key(CompanyId) references Company(company_Id)
+)
+Insert into Employee values('Veer','M','7812453698','Mumbai','2020-10-07','1'),('Venkat','M','7214587875','Banglore','2019-05-08','2'),('Priyanka','F','7345787969','Chennai','2021-01-17','2'),('Sushmita','F','9814753647','Mysore','2017-12-12','2');
+select * from Employee;
+
+--Create table---Payroll table---
+create table payroll
+(
+empId int 
+foreign key(empId) references Employee(EmployeeId),
+BasicPay float,
+TaxablePay float,
+IncomeTax float,
+Deductions float,
+NetPay float
+)
+Insert into payroll(empId,BasicPay,IncomeTax,Deductions)values('1','950000','30000','25000'),('2','550000','20000','20000'),('3','450500','19000','20000'),('4','350500','20000','18400');
+select * from payroll;
+Update payroll set TaxablePay = (BasicPay-Deductions)
+Update payroll set NetPay = (TaxablePay-IncomeTax)
+select * from payroll;
+
+-----Department and employee has many many relation so two diffrent table needed
+---Dept Table---
+create table department_table
+(
+DepartmentId int identity(1,1) primary key,
+DeptName varchar(255)
+)
+insert into department_table values('Developement'),('Marketing'),('HR'),('Sales');
+select * from department_table
+
+----Creating Employee Department table-----
+create table emp_Dept
+(
+EmpId int
+foreign key(EmpId) references Employee(EmployeeId),
+DeptId int
+foreign key(DeptId) references department_table(DepartmentId),
+)
+insert into emp_Dept values('1','3'),('2','1'),('3','4'),('4','3');
+--creating duplicate, means same person works at different dept--
+insert into emp_Dept values('4','2');
+select * from emp_Dept;
+
